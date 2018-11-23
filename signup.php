db9e8c9f67dfc9beba939fdc3543b8aa7f4e9340
affenddy <waffenddy@gmail.com>
2018-11-23 11:28:03 +0800

    Signup.php

--- signup.php
+++ signup.php
@@ -1,17 +1,19 @@
 <html>
 	<head>
-		<title>Login Page</title>
+		<title>Register Page</title>
 	</head>
 
 	<body>
 		<h2>User Authentication System</h2><hr>
 
-		<h3>Login Form</h3>
+		<h3>Register Form</h3>
 
                 <form method="post" action="">
 
                         <table>
+	
 
+				<tr><td>Email:</td> <td><input type="text" value=""></td></tr>
                                 <tr><td>Username:</td><td><input type="text" value=""</td></tr>
                                 <tr><td>Password:</td><td><input type="password" value=""</td></tr>
                                 <tr><td></td><td><input style="float: right:" type="submit" value="Signin"></td></tr>
